164c233ceaa0f2bbc48c410c65fe8e9c64020df7
alem <woubshet@mail.com>
2023-01-04 17:36:22 -0500

    GetMapping is added

--- src/main/java/com/alem/springjenkins/controller/StudentController.java
+++ src/main/java/com/alem/springjenkins/controller/StudentController.java
@@ -1,11 +1,13 @@
 package com.alem.springjenkins.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/students")
 public class StudentController {
+    @GetMapping
     public String getStudent(){
         return "Hello Alem";
     }
