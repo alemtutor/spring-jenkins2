35b5ae7d8292ed257d4d8679b19077a765cc52bb
alem <woubshet@mail.com>
2023-01-04 17:01:08 -0500

    Initial commit

---
+++ src/main/java/com/alem/springjenkins/controller/StudentController.java
@@ -0,0 +1,12 @@
+package com.alem.springjenkins.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+    public String getStudent(){
+        return "Hello Alem";
+    }
+}
